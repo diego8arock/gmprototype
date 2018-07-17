642fb8f4e3034dd45e757b4c1988ca8d5cc43795
DIEGO-PC\Diego <diegoa2000alt@gmail.com>
2018-07-16 20:04:36 -0500

    Add attributes interface

--- utils/attributes/attribute.gd
+++ utils/attributes/attribute.gd
@@ -1,4 +1,19 @@
 extends Node
 
-func _ready():
+var level
+var modifier
+
+func init(level_init, modifier_init):
+	level = level_init
+	modifier = modifier_init
+
+func increase_level(increase):
+	level += increase
+	adjust_modifier(level)
+
+func adjust_modifier(level):
 	pass
+
+func apply_modifier(value):
+	pass
+
