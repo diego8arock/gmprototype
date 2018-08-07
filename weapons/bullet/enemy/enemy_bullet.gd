6875dd066a9962e01ae72124624a2a10a881aeeb
DIEGO-PC\Diego <diegoa2000alt@gmail.com>
2018-08-06 21:16:36 -0500

    Add enemy bullet scene

---
+++ weapons/bullet/enemy/enemy_bullet.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var vel = Vector2()
+export var speed = 1000
+export var damage = 10
+
+func start_at(bullet_direction, bullet_position):
+	rotation = bullet_direction
+	position = bullet_position
+	vel = Vector2(0, speed)
+
+func _process(delta):
+	position = position + vel * delta
+
+func _on_LifeTime_timeout():
+	queue_free()
