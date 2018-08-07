65612e88901267ca9fabf5851b973f0bf46077fe
DIEGO-PC\Diego <diegoa2000alt@gmail.com>
2018-08-06 21:17:11 -0500

    Add player positon global variable

--- utils/autoload/global_variable.gd
+++ utils/autoload/global_variable.gd
@@ -1,15 +1,12 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var player_node
+var player_global_position
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	for node in get_tree().get_nodes_in_group("actors"):
+		if node.name == "Player":
+			player_node = node
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	player_global_position = player_node.global_position
