5bbacfd5dfd05a4776d29b824800af84d4e9b906
DIEGO-PC\Diego <diegoa2000alt@gmail.com>
2018-09-22 18:57:00 -0500

    Add UI for energy and life bar

--- ui/bars/life_bar/life_bar.gd
+++ ui/bars/life_bar/life_bar.gd
@@ -2,22 +2,19 @@ extends HBoxContainer
 
 signal maximum_changed(maximum)
 
-var maximum = 100
-var current_health = 0
+var maximum = GlobalConstant.PLAYER_MAX_LIFE
+var current_health = GlobalConstant.PLAYER_MAX_CHARGE
 
 func initialize(max_value):
 	maximum = max_value
 	emit_signal("maximum_changed", maximum)
 
-func _on_Interface_health_updated(new_health):
-	animate_value(current_health, new_health)
-	update_count_text(new_health)
-	current_health = new_health
-
 func animate_value(start, end):
 	$Tween.interpolate_property($TextureProgress, "value", start, end, 0.5, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
-	$Tween.interpolate_method(self, "update_count_text", start, end, 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.start()
-	if end < start:
-		$AnimationPlayer.play("shake")
+#	if end < start:
+#		$AnimationPlayer.play("shake")
 
+func _on_Interface_life_updated(new_life):
+	animate_value(current_health, new_life)
+	current_health = new_life
--- ui/interface.gd
+++ ui/interface.gd
@@ -1,9 +1,14 @@
 extends Control
 
 signal energy_updated(new_energy)
+signal life_updated(new_life)
 
 func _ready():
-	get_node("Bars/EnergyBar").initialize(GlobalConstant.PLAYER_MAX_CHARGE)
+	$Bars/EnergyBar.initialize(GlobalConstant.PLAYER_MAX_CHARGE)
+	$Bars/LifeBar.initialize(GlobalConstant.PLAYER_MAX_LIFE)
 
 func _on_Player_ui_energy_updated(energy):
 	emit_signal("energy_updated", energy)
+
+func _on_Player_ui_life_updated(value):
+	emit_signal("life_updated", value)
